c32323f9cf30ab5de9ccc077171ccb98eafb2ae0
Nick Krishnan <nkrishnan10@gmail.com>
2020-05-13 15:41:40 -0600

    separate table view into its own vc

--- Task Hack/ViewController.swift
+++ Task Hack/ViewController.swift
@@ -8,43 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController {
 
 
     @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
-            super.viewDidLoad()
-
-            for i in 0...1000 {
-                 data.append("\(i)")
-            }
-
-            self.tableView.dataSource = self
-            self.tableView.delegate = self
-       }
-
-    private var data: [String] = []
-
-
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-         let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")! //1.
-
-         let text = data[indexPath.row]
-
-         cell.textLabel?.text = text
-
-         return cell
-    }
-
-
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-         return data.count
+        super.viewDidLoad()
+        // do additional setup here
     }
 
 }
