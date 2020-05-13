1216e4938976f74318d62586c4840fc8ce0be2fe
Nick Krishnan <nkrishnan10@gmail.com>
2020-05-13 15:20:28 -0600

    init tableview in main vc

--- Task Hack/ViewController.swift
+++ Task Hack/ViewController.swift
@@ -8,13 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+            super.viewDidLoad()
+
+            for i in 0...1000 {
+                 data.append("\(i)")
+            }
+
+            self.tableView.dataSource = self
+            self.tableView.delegate = self
+       }
+
+    private var data: [String] = []
+
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+         let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")! //1.
+
+         let text = data[indexPath.row]
+
+         cell.textLabel?.text = text
+
+         return cell
+    }
+
+
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+         return data.count
+    }
 
 }
 
